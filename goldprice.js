b39aab466368199f7ef7126998168dcf1290c30a
Nithesh <selvarajselvaraj9995@gmail.com>
2026-02-22 21:59:31 +0530

    Add files via upload

---
+++ goldprice.js
@@ -0,0 +1,32 @@
+let loading=true;
+
+let loadingtext=document.getElementById('loadingtext')
+
+
+async function getdata() {
+
+try{
+ loadingtext.style.display="block";
+   const res=await fetch("https://api.gold-api.com/price/XAU")
+    const data= await res.json()
+loadingtext.style.display="none";
+}
+catch(error){
+    loadingtext.innerHTML="<h1> loading failed </h1>"
+
+}
+  
+
+
+
+}
+
+let  data1 = getdata();
+
+
+
+
+
+
+
+
